89d77d7f1929ad4758c6fa092d50cc3fb24fc66d
John Mettraux <jmettraux@gmail.com>
2009-03-04 23:17:47 +0900

    updated test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,8 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'test_help'
 
-class Test::Unit::TestCase
+#class Test::Unit::TestCase
+class ActiveSupport::TestCase
 
   include AuthenticatedTestHelper
 
